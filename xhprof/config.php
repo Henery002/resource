262f735d7305c8a187d42a415bc2c3714785be0d
821901008@qq.com <821901008@qq.com>
2016-11-19 17:39:14 +0800

    add session

--- xhprof/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
- * Set the absolute paths on your system
- */
-define('ERROR_FILE', 	'D:/html/xhprof/logs');
-define('TMP_DIRECTORY', 'D:/html/xhprof/logs');
-define('DOT_BINARY', 	'C:/Program Files (x86)/Graphviz2.38/bin/dot');
-?>
